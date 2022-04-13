6f58d23b6e03320519c28e28a7e0d797587546e7
Shun Hao <shunh@nvidia.com>
2022-04-14 00:12:09 +0800

    update for basic hello
    Signed-off-by: Shun Hao <shunh@nvidia.com>

--- src/hello.rs
+++ src/hello.rs
@@ -3,17 +3,29 @@ use rand::Rng;
 //use std::cmp::Ordering;
 use std::mem;
 
+pub fn type_of<T>(_: &T) -> &'static str {
+    std::any::type_name::<T>()
+}
+
+
+/*You declare constants using the const keyword instead of the let keyword,
+    and the type of the value must be annotated. */
+const _MAX : i32 = 101;
+
 pub fn log_hello() {
     println!("*******************Chapter 1~6***********************");
 }
 
 pub fn hello() {
     /**************basic variable define*************************/
+    let boolv : bool = true;
+    let charv : char = 'a';
     let test = "    ";
-    println!("...{}...", test);
+    println!("...{}{}{}...", boolv, test, charv);
     let mut test: usize = test.len();
     test = test + 1;
     println!("len:{}", test);
+
 
     /***************array and tuple************************/
 
@@ -25,7 +37,7 @@ pub fn hello() {
     months[0] = "Jan"; // this is array indexing method only, cannot be used on tuple
     let array_a: [u32; 5] = [0; 5];
     index = index + 1;
-    println!("month: {} {}", months[0], array_a[index]);
+    println!("month: {} array_a[{}]:{}", months[0], index, array_a[index]);
 
     // tuple in Rust has a FIXED length. can have different types, value can be changed(cannot change type)
     let mut tup_v = (500, "test", 'z', array_a);
@@ -87,11 +99,7 @@ let __val: u32 = rand::thread_rng().gen_range(1, 111);
 fn another_function(mut param1: u32) -> [u32; 1] {
     println!("*******************Chapter 3***********************");
     let condition = true;
-    let number = if condition {
-        10
-    } else {
-        6
-    };
+    let number = if condition {10} else {6};
 
     println!("param: {}", param1);
     param1 = 999;
@@ -111,7 +119,7 @@ fn another_function(mut param1: u32) -> [u32; 1] {
         counter += 1 * (1 + 1);
         println!("++ {}", counter);
         if counter == 10 {
-            break counter * 2;
+            break counter * 2; // break is like return
         }
     };
 
@@ -120,22 +128,52 @@ fn another_function(mut param1: u32) -> [u32; 1] {
         counter -= 2;
     };
 
+    let mut total = 0;
 
+    'out_loop : loop {
+        counter = 0;
+        total += 1;
+        if total > 5 {
+            println!("total: {}", total);
+            break;
+        }
+        loop {
+            counter += 1;
+            if counter == 5 {
+                break 'out_loop;
+            }
+        };
+    };
+    println!("total: {}", total);
+
+    // this is like using: return [1];
     [1]
 }
 
 /******************** Chapter 4 *******************/
+/* The ownership noly affects the variable on heap. For basic variable types:
+    1. u8, u32 etc.
+    2. bool
+    3. f64
+    4. char, &str (?)
+    5. tuple/array. (with scalar members only)
+    a = b, always on stack and copy, no ownership. */
 fn ownership_test() {
     println!("*******************Chapter 4***********************");
-    /***************move*****************/
-    let xx = String::from("90");
+    /* for basic scalar, &str (pointer to const string in static data segment),
+       it's, always copy, xx not moved to yy. */
+    let xx = "90";
     let yy = xx;
-    println!("string1 is {}", yy);
-    //println!("yy is {}", xx); // xx has been moved to yy, xx cannot be used anymore (already been freed)
+    println!("### const &str type: xx is {}, yy is {}, type_of : xx {}, yy {}", xx, yy, type_of(&xx), type_of(&yy));
+    /* For String, it's in heap. */
+    let xxx = String::from("900");
+    let yyy = xxx;
+    // xxx has been moved to yyy, xxx cannot be used anymore (already been freed)
+    println!("### String, ownership moved, yyy is {}, type_of :yyy {}", yyy, type_of(&yyy));
 
     let s1 = String::from("teststring");
     let s2 = s1.clone();
-    println!("string2 is {} {}", s2, s1); // s1 has been deep cloned to s2, so can use both
+    println!("### String, cloned. s1:{}, s2:{}", s2, s1); // s1 has been deep cloned to s2, so can use both
 
     let ss3 = String::from("for function");
     take_ownership(ss3);
@@ -168,6 +206,17 @@ fn ownership_test() {
         where they are last used, which is before the mutable reference r3 is created. 
         These scopes don’t overlap, so this code is allowed. */
     }
+
+    let arr1 = [1,2,3,4,5];
+    let take_arr = &arr1;
+    println!("output arr:{}", take_arr[1]);
+    println!("output arr:{}", arr1[0]);
+    // array is on stack, so not moved.
+    func_array(&arr1);
+}
+
+fn func_array(in_arr: &[u32; 5]) {
+    println!("func_array output :{}", in_arr[2]);
 }
 
 fn take_ownership(input: String) {
@@ -175,7 +224,8 @@ fn take_ownership(input: String) {
 }
 
 fn takes_and_gives_back(input: String) -> String {
-    input
+    //input
+    return input;
 }
 
 fn borrow_test(param: &mut String) -> usize {
@@ -185,6 +235,13 @@ fn borrow_test(param: &mut String) -> usize {
 
 fn slice_test() {
     let mut ss = String::from("slice string");
+    /* &str type can point (reference) to String in heap, like:
+            &ss, this is reference of ss, so it's a &str
+            &ss[..], &ss[0..1] ... they are slice (partial/all reference), also &str
+       &str type can also point to const string in static area:
+            let tmps = "Test",   this is also &str, just immutable
+    */
+
     //The type of ss here is &str: it’s a slice pointing to that specific point of the binary
 
     println!("slice string: {}", ss);
@@ -205,11 +262,19 @@ struct User {
 }
 
 impl User {
-    fn inc_cnt(&mut self, val: u32) {
+    // Associated Functions examples
+
+    fn inc_cnt(&mut self, val: u32) -> User {
         self.count += val;
+
+        let res = User {
+            email : String::from("111"),
+            username : String::from("222"),
+            count : 333
+        };
+        return res;
     }
 
-    // Associated Function
     fn create_user(email: String, username: String, count: u32) -> User {
         User {
             email,
@@ -232,9 +297,11 @@ struct Point(i32, i32, i32);
 fn struct_test() -> Point {
     println!("*******************Chapter 5***********************");
 
+    // it's on stack by default. Use Box::new() for allocating on heap.
     let point = Point(1,2,3);
-    // by default, Rust's assignment moves the object, making the old location invalid
+    // by default, Sturct uses moving for the object (though on stack), making the old location invalid
     //let movp = point; // will move point
+    // dbg!(&point);
     println!("point: {:?}, size: {}", point, mem::size_of_val(&point));
     println!("point: {} {} {}", point.0, point.1, point.2);
 
@@ -265,9 +332,10 @@ fn struct_test() -> Point {
 
     let stru2 = User {
         username: String::from("stru2_name"),
-        ..stru // will do partial move, email has been moved
+        ..stru // must be the last line. // will do partial move, email has been moved
     };
-    //println!("...... {}", stru.email); // stru partial moved, cannot use email
+    // println!("...... {}", stru.email); // stru partial moved, cannot use email
+    println!("...... {}", stru.username); // stru partial moved, can use username
     println!("stru2 test {:#?}", stru2);
     stru.email = stru2.email; // recover stru
 
@@ -282,19 +350,19 @@ fn struct_test() -> Point {
     let new_user = User::create_user(String::from("new@new.com"), String::from("newname"), 0);
     println!("{:?}", new_user);
 
-    // struct Point point is on heap
+    // Can return local struct, actually return value on stack.
     return point;
-
 }
 
 
 /******************** Chapter 6 *******************/
 // can associate data to each variant of the enum, so there is no need for an extra struct.
 // and each variant can have different types and amounts of associated data.
+#[derive(Debug)]
 enum _IpAddr {
-    V4(u8, u8, u8, u8), // associate four u8 data to V4
-    V6(String), // associate one String to V6
-    Empty,  // associate nothing
+    _V4(u8, u8, u8, u8), // associate four u8 data to V4
+    _V6(String), // associate one String to V6
+    _Empty,  // associate nothing
 }
 
 #[derive(Debug)]
@@ -325,7 +393,13 @@ fn enum_test() {
 
     en1.print_val();
     en2.print_val();
+    en3.print_val();
     //println!("is AA: {}", en1 == MyEnum::AA);
+
+    let ip4 = _IpAddr::_V4(1, 2, 3, 4);
+    let ip4_1 = ip4;
+    // Enum also using moving, so ip4 cannot be used now.
+    println!("xxx {:#?}", ip4_1);
 }
 
 fn option_test() {
@@ -351,6 +425,11 @@ fn option_test() {
     option_test1(a);
     option_test1(c);
     //option_test1(_b); // will compile error cause None
+
+    let _aa = a;
+    if let Some(5) = a {
+        println!("xxxxxxxx can use a"); // Some is using Copy attribute, so using deep copy, not moving
+    }
 
     let sa = Some(String::from("straa"));
     //_option_test11(sa); // will move ownership here
